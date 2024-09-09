c960a63910b5ceae157dae44352f0b4fbb0de3ea
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2024-09-09 13:03:01 -0300

    add atualizacao pastas projeto

---
+++ site_sintonize/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from requests.exceptions import Timeout
+import requests
+
+def index (request):
+    return render (request, 'index.html')
+
+def politicas_privacidade (request):
+    return render(request, 'politicas_privacidade.html')
+
+def sondagem (requests):
+    return render (requests, 'sondagem.html')
