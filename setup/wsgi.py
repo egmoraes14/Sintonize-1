5826fd003c8e845adf9b29187e99dc32feddd7d9
egmoraes14 <egmoraes14@gmail.com>
2024-09-11 18:48:23 -0300

    Merge branch 'main' of https://github.com/ProjetoSafira/Sintonize

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings', 'site_sintonize.settings')
 
 application = get_wsgi_application()
